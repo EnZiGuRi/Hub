a6e75c04d86b83fa003f91fad71451e9346f0d29
EnZiGuRi <admin@legendsmc.org>
2016-08-27 19:30:39 +0100

    Added Config File

--- src/me/LegendsMC/Hub/Hub.java
+++ src/me/LegendsMC/Hub/Hub.java
@@ -1,5 +1,62 @@
 package me.LegendsMC.Hub;
 
-public class Hub {
+import java.io.File;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Hub extends JavaPlugin {
+	private static Plugin instance;
+
+	boolean DebugMode = getConfig().getBoolean("DebugMode");
+
+	public void onEnable() {
+		instance = this;
+		initializeConfig();
+		registerEvents();
+		registerCommands();
+		if (DebugMode == true) {
+			Bukkit.getConsoleSender().sendMessage(
+					ChatColor.GREEN + "[Hub] " + ChatColor.RED
+							+ "Debug Mode ON");
+		}
+		Bukkit.getConsoleSender().sendMessage(
+				ChatColor.GREEN + "[Hub] Enabled!");
+	}
+
+	public void onDisable() {
+		Bukkit.getConsoleSender().sendMessage(
+				ChatColor.GREEN + "[Hub] Disabled!");
+	}
+
+	public void initializeConfig() {
+		File configfile = new File(getDataFolder() + File.separator
+				+ "config.yml");
+		if (!configfile.exists()) {
+			Bukkit.getConsoleSender().sendMessage(
+					ChatColor.GREEN
+							+ "[Hub] Generating config files...");
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}
+	}
+
+	public void registerEvent(Listener listener) {
+		getServer().getPluginManager().registerEvents(listener, this);
+	}
+
+	private void registerEvents() {
+		//registerEvent(new SignCreateEvent());
+	}
+
+	private void registerCommands() {
+		//getCommand("RewardTransfer").setExecutor(new CheckDB());
+	}
+
+	public static Plugin getInstance() {
+		return instance;
+	}
 }
